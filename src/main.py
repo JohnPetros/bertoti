cf0b44a03f8ffc0a1e62dcf33ac762922d86bbdf
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-15 20:50:28 -0300

    ⚙️ config: allow all origins for CORS

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 from dotenv import load_dotenv
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.controllers import ListMessagesController, SendMessageController
 
 load_dotenv()
 
-
 app = FastAPI()
+router = APIRouter(prefix="/chatbot")
 
-ListMessagesController(app)
-SendMessageController(app)
+ListMessagesController(router)
+SendMessageController(router)
+
+app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
